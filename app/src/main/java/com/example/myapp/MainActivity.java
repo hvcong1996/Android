2cceb7822ca2df09eda7bc9e694c04aaceaf5c4f
hvcong <hvcong@tma.com.vn>
2021-03-01 10:36:30 +0700

    Code learn Khoa Pham: - Investigate the item EditText

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -5,27 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtContent;
-    Button btnChangeText;
+//    TextView txtContent;
+//    Button btnChangeText;
+    EditText edtUserName;
+    Button btnGetUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtContent = (TextView) findViewById(R.id.textViewData);
-        btnChangeText = (Button) findViewById(R.id.buttonChangeText);
+//        txtContent = (TextView) findViewById(R.id.textViewData);
+//        btnChangeText = (Button) findViewById(R.id.buttonChangeText);
+//
+//        btnChangeText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                txtContent.setText("Text changed");
+//            }
+//        });
 
-        btnChangeText.setOnClickListener(new View.OnClickListener() {
+        edtUserName = (EditText) findViewById(R.id.editTextTextPersonName);
+        btnGetUserName = (Button) findViewById(R.id.buttonGetPersonName);
+
+        btnGetUserName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtContent.setText("Text changed");
+                String content = edtUserName.getText().toString();
+                Toast.makeText(MainActivity.this, content, Toast.LENGTH_LONG).show();
             }
         });
     }
